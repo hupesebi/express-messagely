2456ccd8b4762eaddb55e782da86bc8dff1ebd78
Sebastian Hupe <Sebastian.Hupe@gmail.com>
2021-10-11 13:48:56 +0200

    Completed messagely exercise

---
+++ routes/messages.js
@@ -0,0 +1,89 @@
+
+const express = require("express");
+const router = express.Router();
+const { ensureLoggedIn } = require("../middleware/auth");
+const Message = require("../models/message");
+const ExpressError = require("../expressError");
+const { DB_URI } = require("../config");
+
+/** GET /:id - get detail of message.
+ *
+ * => {message: {id,
+ *               body,
+ *               sent_at,
+ *               read_at,
+ *               from_user: {username, first_name, last_name, phone},
+ *               to_user: {username, first_name, last_name, phone}}
+ *
+ * Make sure that the currently-logged-in users is either the to or from user.
+ *
+ **/
+ router.get("/:id", ensureLoggedIn, async (req, res, next) => {
+    try {
+      const { username } = req.user;
+  
+      const message = await Message.get(req.params.id);
+  
+      if (message.from_user === username || message.to_user === username) {
+        return res.json({ message });
+      } 
+      throw new ExpressError("Unauthorized", 401);
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+/** POST / - post message.
+ *
+ * {to_username, body} =>
+ *   {message: {id, from_username, to_username, body, sent_at}}
+ *
+ **/
+router.post('/', ensureLoggedIn, async(req, res, next) => {
+   try{
+    const {to_username, body} = req.body
+    if (!username || !body){
+        throw new ExpressError ('Username and body required', 400 )
+    }
+
+    const from_username = req.user.username
+    if (to_username === from_username){
+        throw new ExpressError ('Not permitted sending messages to yourself', 400)
+    }
+
+    const message = await Message.create({from_username, to_username, body})
+    return res.json({message})
+
+   }catch(e){
+       return next(e)
+   }
+   
+})
+
+/** POST/:id/read - mark message as read:
+ *
+ *  => {message: {id, read_at}}
+ *
+ * Make sure that the only the intended recipient can mark as read.
+ *
+ **/
+
+ router.post("/:id/read", ensureLoggedIn, async (req, res, next) => {
+    try {
+      const { username } = req.user;
+      const message = await Message.get(req.params.id);
+      if (message.to_user.username === username) {
+        const read = await Message.markRead(req.params.id);
+        if (read) {
+          return res.json({ status: "message read" });
+        }
+      }
+      throw new ExpressError("Unauthorized", 401);
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+module.exports = router;
+
+
